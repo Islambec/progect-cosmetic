8c792f6b6003928be830c753c4667fbed5126c3b
Islambec <islamkulcoroev457@gmail.com>
2024-08-02 07:17:30 +0300

    add new function map in js

--- script/index.js
+++ script/index.js
@@ -9,3 +9,45 @@ for(let i = 0; i < links.length; i++) {
     }
 }
 
+const products = [
+    {
+        id: 1,
+        name: "Cosmetic products",
+        descrition: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+        image: "./assets/image/pump-bottle-02 1.png",
+        price: 120,
+    },
+    {
+        id: 2,
+        name: "Cosmetic products",
+        descrition: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+        image: "./assets/image/jar-15 1.png",
+        price: 120,
+    },
+];
+
+const productsBox = document.querySelector(".products-box");
+
+const productsMap = products.map((el) => {
+    return `
+    <div class="card">
+        <div class="card-image">
+            <img src="${el.image}" alt="pump image" loading="lazy">
+        </div>
+        <div class="card-content">
+            <h2>${el.name}</h2>
+            <p>${el.descrition}</p>
+            <div class="card-actions">
+                <div class="card-action">
+                    <button>Buy Now</button>
+                    <button>Add To Cart</button>
+                </div>
+                <span>${el.price}</span>
+            </div>
+        </div>
+    </div>
+    `
+});
+
+productsBox.innerHTML = productsMap.join("");
+console.log(productsMap);
